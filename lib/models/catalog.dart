ecaf5e2f024dd7e565bc43246f7cb3ff78ba7b7d
vashishtha <vashishthasiyaram@gmail.com>
2021-04-26 23:09:11 +0530

    Model added

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item{
+  final String id;
+  final String name;
+  final String des;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Item({this.id, this.name, this.des, this.price, this.color, this.imageUrl});
+}
+
+final products= [
+  Item(
+    id: "1", 
+    name: "", 
+    des: "", 
+    price: 100, 
+    color: "", 
+    imageUrl: ""
+  )
+];
