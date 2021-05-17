2795814d76be19e4d7dbe4a04b981f875a7535e7
vashishtha <vashishthasiyaram@gmail.com>
2021-05-17 16:52:05 +0530

    GridVew for list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(Duration(seconds:4));
+    await Future.delayed(Duration(seconds: 4));
     var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     print("a1" + catalogJson);
     var decodedData = jsonDecode(catalogJson);
@@ -47,14 +47,36 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                // itemCount: dummyList.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 10,
+                    crossAxisCount: 2),
                 itemBuilder: (context, index) {
-                  return ItemWidget(item: CatalogModel.items[index]
-                      // item: dummyList[index]
-                      );
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          header: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(),
+                              child: Text(
+                                item.name,
+                                style: TextStyle(color: Colors.white),
+                              )),
+                          footer: Text(item.price.toString()),
+                          child: Image.network(item.image)));
                 })
+            // ListView.builder(
+            //     itemCount: CatalogModel.items.length,
+            //     // itemCount: dummyList.length,
+            //     itemBuilder: (context, index) {
+            //       return ItemWidget(item: CatalogModel.items[index]
+            //           // item: dummyList[index]
+            //           );
+            //     })
             : Center(
                 child: CircularProgressIndicator(
                   backgroundColor: Colors.red,
